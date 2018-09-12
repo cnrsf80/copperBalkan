9b8c5f0d50617527c4ec2542302ebde6c9f70a49
hhoareau <hhoareau@gmail.com>
2018-09-12 20:06:07 +0200

    version avec separation

--- main.py
+++ main.py
@@ -7,6 +7,25 @@ import pandas as pd
 from networkx.algorithms import community
 
 
+class cluster:
+    name=""
+    labels=[]
+    color="red"
+
+    def __init__(self, name, index,nodes, color):
+        self.nodes = []
+        self.name = name
+        for i in index:
+            self.nodes.append(nodes[i])
+            self.labels.append(i)
+        self.color= color
+
+    def contain(self,i):
+        for n in nodes:
+            if n.ix == i:return True
+
+
+
 #Definitions
 def distance(i,j):
     return np.linalg.norm(i.values - j.values).round(0)
@@ -14,11 +33,11 @@ def distance(i,j):
 v_seuil=np.vectorize(lambda x,y:1 if x<y else 0)
 
 
-def create_matrix(seuil):
-    if os.path.isfile("adjacence_artefact.csv"):
+def create_matrix(seuil,limit):
+    if os.path.isfile("adjacence_artefact.csv") and limit>200:
         matrice=np.asmatrix(np.loadtxt("adjacence_artefact.csv",delimiter=" "),dtype=np.int16)
     else:
-        data = pd.read_excel("cnx013_supp_table_s1.xlsx")
+        data = pd.read_excel("cnx013_supp_table_s1.xlsx").head(limit)
         composition = data.loc[:, "As (ppm)":"Se (ppm)"]
         matrice = np.asmatrix(np.zeros((len(data),len(data))),dtype=np.int16)
 
@@ -39,46 +58,51 @@ def create_matrix(seuil):
     return matrice_ww
 
 
-G=nx.from_numpy_matrix(create_matrix(0))
-partition = community.kernighan_lin_bisection(G,None,10,weight='weight')
+def trace_artefact(G,partitions):
+    colors = ["red", "green", "blue", "yellow", "grey", "purple", "orange"]
+    pos = nx.spring_layout(G)
+    for i in range(0, len(partitions)):
+        c = cluster("", partitions[i], list(G.nodes), colors[i])
+        nx.draw_networkx_nodes(G, pos, c.nodes, label=c.labels, node_size=20, node_color=c.color)
 
-#drawing
-size = float(len(set(partition.values())))
-pos = nx.spring_layout(G)
-count = 0.
-for com in set(partition.values()) :
-    count = count + 1.
-    list_nodes = [nodes for nodes in partition.keys()
-                                if partition[nodes] == com]
-    nx.draw_networkx_nodes(G, pos, list_nodes, node_size = 20,
-                                node_color = str(count / size))
+    nx.draw_networkx_edges(G, pos, alpha=0.5)
 
-
-nx.draw_networkx_edges(G,pos, alpha=0.5)
-plt.show()
+    plt.axis('off')
+    plt.savefig('graph.pdf', format="pdf")
+    plt.show()
 
 
-# pos=nx.spring_layout(G)
-#
-# nx.draw(G,node_size=5,node_color="blue")
-#
-# plt.axis('off')
-# plt.savefig('filename.png', dpi = 300)
+def create_site_matrix(clusters,limit):
+    data = pd.read_excel("cnx013_supp_table_s1.xlsx").head(limit)
+    sites=data.loc[:"Site":"Type of site/context"]
+    sites.drop_duplicates()
+    M = np.asmatrix(np.zeros((len(sites), len(sites))), dtype=np.int16)
+    for i in sites.index:
+            for j in sites.index:
+                if(M[i,j]==0 and i!=j):
+                    for c in clusters:
+                        if c.contain(i) and c.contain(j):
+                            M[i,j]=1
+                            M[j,i]=1
 
-#
-# partition=best_partition(G)
-#
-# #drawing
-# size = float(len(set(partition.values())))
-# pos = nx.spring_layout(G)
-# count = 0.
-# for com in set(partition.values()) :
-#     count = count + 1.
-#     list_nodes = [nodes for nodes in partition.keys()
-#                                 if partition[nodes] == com]
-#     nx.draw_networkx_nodes(G, pos, list_nodes, node_size = 5,
-#                                 node_color = str(count / size))
-#
-#
-# nx.draw_networkx_edges(G,pos, alpha=0.5)
-# plt.show()
+
+
+limit=50
+n_community=4
+
+G=nx.from_numpy_matrix(create_matrix(700,limit))
+#partition = community.kernighan_lin_bisection(G,None,20,weight='weight')
+
+#comp=community.girvan_newman(G)
+#partition = tuple(sorted(c) for c in next(comp))
+
+coms=community.asyn_fluidc(G,n_community,100)
+partition=[]
+for c in coms:
+    partition.append(c)
+
+create_site_matrix(partition,limit)
+
+
+
+
