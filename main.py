8ffe490b752b65524ba96f3680265cfa0211cda5
hhoareau <hhoareau@gmail.com>
2018-09-13 20:59:04 +0200

    version avec separation

--- main.py
+++ main.py
@@ -4,25 +4,31 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 import pandas as pd
+import folium
 from networkx.algorithms import community
 
+colors = ["red", "green", "blue", "yellow", "grey", "purple", "orange"]
 
 class cluster:
     name=""
-    labels=[]
     color="red"
 
-    def __init__(self, name, index,nodes, color):
-        self.nodes = []
+    def __init__(self, name,index=[]):
+        self.index = index
         self.name = name
-        for i in index:
-            self.nodes.append(nodes[i])
-            self.labels.append(i)
-        self.color= color
 
     def contain(self,i):
-        for n in self.nodes:
-            if n.ix == i:return True
+        for n in self.index:
+            if n == i:
+                return True
+        return False
+
+    def add_index(self,index):
+        self.index.append(index)
+
+    def print(self,data,label_col=""):
+        print("\nCluster:"+self.name)
+        print(" + ".join(data[label_col][self.index]))
 
 
 
@@ -33,11 +39,10 @@ def distance(i,j):
 v_seuil=np.vectorize(lambda x,y:1 if x<y else 0)
 
 
-def create_matrix(seuil,limit):
-    if os.path.isfile("adjacence_artefact.csv") and limit>200:
+def create_matrix(data,seuil):
+    if os.path.isfile("adjacence_artefact.csv") and len(data)>149:
         matrice=np.asmatrix(np.loadtxt("adjacence_artefact.csv",delimiter=" "),dtype=np.int16)
     else:
-        data = pd.read_excel("cnx013_supp_table_s1.xlsx").head(limit)
         composition = data.loc[:, "As (ppm)":"Se (ppm)"]
         matrice = np.asmatrix(np.zeros((len(data),len(data))),dtype=np.int16)
 
@@ -58,55 +63,126 @@ def create_matrix(seuil,limit):
     return matrice_ww
 
 
-def trace_artefact(G,partitions):
-    colors = ["red", "green", "blue", "yellow", "grey", "purple", "orange"]
+def trace_artefact(G,clusters,data,label_col=""):
     pos = nx.spring_layout(G)
-    for i in range(0, len(partitions)):
-        c = cluster("", partitions[i], list(G.nodes), colors[i])
-        nx.draw_networkx_nodes(G, pos, c.nodes, label=c.labels, node_size=20, node_color=c.color)
 
-    nx.draw_networkx_edges(G, pos, alpha=0.5)
+    if len(clusters)>0:
+        i=0
+        for c in clusters:
+            labels=data[label_col][c.index]
+            nx.draw_networkx_nodes(G, pos, c.index, label=labels, node_size=20, node_color=colors[i])
+            i=i+1
+
+        nx.draw_networkx_edges(G, pos, alpha=0.5)
+    else:
+        nx.draw(G)
 
     plt.axis('off')
     plt.savefig('graph.pdf', format="pdf")
     plt.show()
 
 
-def create_site_matrix(clusters,limit):
-    data = pd.read_excel("cnx013_supp_table_s1.xlsx").head(limit)
-    sites=data.loc[:,"Site":"Type of site/context"]
-    sites.drop_duplicates()
+def create_sites_df(data):
+    sites = data.loc[:, "Site":"Type of site/context"]
+    sites=sites.drop_duplicates("Site")
+    return sites
+
+def create_site_matrix(data,artefact_clusters):
+    sites=create_sites_df(data)
     M = np.asmatrix(np.zeros((len(sites), len(sites))), dtype=np.int16)
-    for i in sites.index:
-            for j in sites.index:
-                if(M[i,j]==0 and i!=j):
-                    for c in clusters:
-                        if c.contain(i) and c.contain(j):
-                            M[i,j]=1
-                            M[j,i]=1
+    liste_sites = list(sites.Site)
+    for c in artefact_clusters:
+        for s_i in data.Site[c.index]:
+            for s_j in data.Site[c.index]:
+                if s_i!=s_j:
+                    M[liste_sites.index(s_i),liste_sites.index(s_j)]=1
+                    M[liste_sites.index(s_j), liste_sites.index(s_i)] = 1
+
+    return M
 
 
+def create_clusters_from_girvannewman(G):
+    comp=community.girvan_newman(G)
+    clusters=[]
+    for partition in list(sorted(c) for c in next(comp)):
+        clusters.append(cluster("girvan_newman",partition))
 
-def create_partition_from_asyncfluid(G,n_community):
-    coms = community.asyn_fluidc(G, n_community, 100)
+    return clusters
+
+
+def create_two_clusters(G:nx.Graph):
+    clusters = []
+    partition = community.kernighan_lin_bisection(G, None, 20, weight='weight')
+
+    i=0
+    for p in partition:
+        clusters.append(cluster("kernighan_lin - "+str(i),p))
+        i=i+1
+    return clusters
+
+
+def create_ncluster(G:nx.Graph,target=4):
+    clusters =[cluster("premier",G.nodes)]
+
+    backup_G=G.copy()
+    while len(clusters) < target:
+       #on cherche le plus grand cluster
+       maxlen=0
+       k=-1
+       for i in range(0,len(clusters)):
+           if len(clusters[i].index)>maxlen:
+               maxlen=len(clusters[i].index)
+               k=i
+        #On divise en deux le plus grand cluster et on le supprime
+       G = backup_G.subgraph(clusters[k].index)
+       clusters.remove(clusters[k])
+       for c in create_two_clusters(G):clusters.append(c)
+
+
+    return clusters
+
+
+def create_clusters_from_asyncfluid(G,n_community):
+    coms = community.asyn_fluidc(G, n_community, 500)
     partition = []
-    for c in coms: partition.append(c)
+    for c in coms:
+        partition.append(cluster("asyncfluid",c))
     return partition
 
-limit=350
-n_community=4
 
-G0=nx.from_numpy_matrix(create_matrix(800,limit))
-nx.draw(G0)
-plt.show()
-#partition = community.kernighan_lin_bisection(G,None,20,weight='weight')
-#comp=community.girvan_newman(G)
-#partition = tuple(sorted(c) for c in next(comp))
+def draw_site_onmap(mymap:folium.Map, G, sites_clusters, sites:pd.DataFrame ,file):
+    for site in sites.iterrows():
+        pos=[site[1]["Geo Latitude"],site[1]["Geo Longitude"]]
+        folium.Marker(pos,site["Site"]).add_to(mymap)
 
-M=create_site_matrix(create_partition_from_asyncfluid(G0,3),limit)
+
+
+
+data = pd.read_excel("cnx013_supp_table_s1.xlsx")
+
+G0=nx.from_numpy_matrix(create_matrix(data,0))
+artefact_clusters=create_ncluster(G0,40);
+
+#artefact_clusters=create_clusters_from_girvannewman(G0);
+
+for c in artefact_clusters:c.print(data,"Sample label")
+#trace_artefact(G0,artefact_clusters,data,"Sample label")
+
+M=create_site_matrix(data,artefact_clusters)
 G=nx.from_numpy_matrix(M)
-partition=create_partition_from_asyncfluid(G,3)
-trace_artefact(G,partition)
+sites_clusters=create_clusters_from_asyncfluid(G,4)
+
+#Affichage
+for c in sites_clusters:c.print(data,"Site")
+trace_artefact(G,sites_clusters,data,"Site")
+
+
+#https://python-visualization.github.io/folium/docs-v0.6.0/
+#mymap=folium.Map(location=[48,2],zoom_start=13)
+#draw_site_onmap(mymap,G,sites_clusters,create_sites_df(data),"map.html")
+
+
+
 
 
 
